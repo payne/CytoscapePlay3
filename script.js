dd1f0beab0d8eaed69c58e78dbe83a4450efaff8
Matt Payne <Payne@MattPayne.org>
2022-12-15 00:17:20 +0000

    Connected buttons to functions

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+let addNodeButton = document.getElementById('addNodeButton');
+let addEdgeButton = document.getElementById('addEdgeButton');
+let addNode = (event) => {
+  console.log(`addNodeButton click`);
+  console.log(event);
+}
+let addEdge = (event) => {
+  console.log(`addEdgeButton click`);
+  console.log(event);
+}
+addNodeButton.addEventListener('click', addNode);
+addEdgeButton.addEventListener('click', addEdge);
 var cy = cytoscape({
   container: document.getElementById('cy'),
   elements: [
     { data: { id: 'a' } },
     { data: { id: 'b' } },
-    {
-      data: {
-        id: 'ab',
-        source: 'a',
-        target: 'b'
-      }
+    { data: { id: 'ab', source: 'a', target: 'b' }
     }]
 });
 window.cy = cy;
