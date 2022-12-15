42fb7b01b5ed7e07257a4e2368f10c26270eb349
Matt Payne <Payne@MattPayne.org>
2022-12-15 22:53:51 +0000

    catching lots of events :-)

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ let cy = cytoscape({
     }]
 });
 window.cy = cy; // so it's easy to play in devtools console
+// https://stackoverflow.com/questions/66516233/change-event-in-cytoscape-js-graph
+cy.on('add dragfree dragfreeon remove move select unselect tapdragover tapselect tapunselect boxselect box lock', generic_event);
+console.log(`setup a bunch of listeners.`);
 let nodes={}; // key is nodeID and value is metadata and list of destination nodeIDs
 let currentNodeId='b';
 let nextNodeId = () => {
@@ -33,12 +36,15 @@ let nodeMovedNew = (event) => {
   console.log(event);
   console.log(nodes);
 } 
+// I want this to be hoisted
+function  generic_event(ev)  { console.log('hey some event happened on cy: ', ev);};
 let addNode = (event) => {
   let nId = nextNodeId();
   let n = cy.add( { data: { id: nId }, position: {  x: 100, y: 100 } });
   console.log(`addNodeButton click`);
   console.log(n);
-  n.on('tapdragover', nodeMoved);
+  // n.on('tapdragover', nodeMoved);
+  // from https://stackoverflow.com/questions/66516233/change-event-in-cytoscape-js-graph
 }
 let addEdge = (event) => {
   console.log(`addEdgeButton click`);
