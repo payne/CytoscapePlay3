2203f03d24522fa70cc7ea094c950272f5fe85bc
payne <>
2022-12-16 00:34:00 +0000

    Getting id of node that was drug (moved).

--- script.js
+++ script.js
@@ -3,27 +3,29 @@ let cy = cytoscape({
   elements: [
     { data: { id: 'a' } },
     { data: { id: 'b' } },
-    { data: { id: 'ab', source: 'a', target: 'b' }
+    {
+      data: { id: 'ab', source: 'a', target: 'b' }
     }]
 });
 window.cy = cy; // so it's easy to play in devtools console
 // https://stackoverflow.com/questions/66516233/change-event-in-cytoscape-js-graph
-cy.on('add dragfree dragfreeon remove move select unselect tapdragover tapselect tapunselect boxselect box lock', generic_event);
-console.log(`setup a bunch of listeners.`);
-let nodes={}; // key is nodeID and value is metadata and list of destination nodeIDs
-let currentNodeId='b';
+// cy.on('add dragfree dragfreeon remove move select unselect tapdragover tapselect tapunselect boxselect box lock', generic_event);
+cy.on('dragfree' , nodeMovedNew);
+let nodes = {}; // key is nodeID and value is metadata and list of destination nodeIDs
+let currentNodeId = 'b';
 let nextNodeId = () => {
-  currentNodeId = String.fromCharCode(currentNodeId[0].charCodeAt()+1);
+  currentNodeId = String.fromCharCode(currentNodeId[0].charCodeAt() + 1);
   return currentNodeId;
 }
 let addNodeButton = document.getElementById('addNodeButton');
 let addEdgeButton = document.getElementById('addEdgeButton');
-let nodeMoved = (event) => { 
+let nodeMoved = (event) => {
   console.log(event);
 }
-let nodeMovedNew = (event) => { 
+function nodeMovedNew(event) {
   console.log(event);
   const id = event.target.id();
+  console.log(`id=${id}`);
   const position = event.position;
   // is id in nodes?
   if (nodes[id]) {
@@ -33,14 +35,11 @@ let nodeMovedNew = (event) => {
     // add id to nodes
     nodes[id] = { position: position, destinations: [] };
   }
-  console.log(event);
   console.log(nodes);
-} 
-// I want this to be hoisted
-function  generic_event(ev)  { console.log('hey some event happened on cy: ', ev);};
+}
 let addNode = (event) => {
   let nId = nextNodeId();
-  let n = cy.add( { data: { id: nId }, position: {  x: 100, y: 100 } });
+  let n = cy.add({ data: { id: nId }, position: { x: 100, y: 100 } });
   console.log(`addNodeButton click`);
   console.log(n);
   // n.on('tapdragover', nodeMoved);
